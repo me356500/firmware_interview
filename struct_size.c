ded1dec20343a71118e5f670062b30bad46429bb
Wei-YiCheng <me356500.cs07@nctu.edu.tw>
2024-09-21 11:03:22 +0800

    feat/struct_size

--- struct_size.c
+++ struct_size.c
@@ -22,10 +22,22 @@ struct tmp3 {
     double c;
 };
 
+struct Some_struct
+{
+    int a;
+    char c[10];
+};
+
 int main() {
     struct tmp1 test1;
     struct tmp2 test2;
     struct tmp3 test3;
 
     printf("%d\n%d\n%d\n", sizeof(test1), sizeof(test2), sizeof(test3));
+
+    // print struct size without using sizeof
+    struct Some_struct ss[2];
+    // char pointer
+    printf("%d\n", (char*)(&ss[1]) - (char*)(&ss));
+
 }
